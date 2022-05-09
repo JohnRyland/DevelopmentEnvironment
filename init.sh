5abafac84a9382131a9c03ccbe25fc9aa9b67100
John Ryland <jryland@gmail.com>
2022-05-10 00:19:19 +1000

    update init.sh with scoop packages

--- init.sh
+++ init.sh
@@ -18,16 +18,21 @@ if [ `uname` == "Linux" ]
 then
   # Install required packages
   sudo apt-get update -y
-  sudo apt install -y g++ git
+
+  # Essentials
+  sudo apt install -y make g++ git
+
+  # Code quality tools
+  sudo apt install -y cppcheck clang-format clang-tidy gcovr
 
   # Install cmake and other tools
   sudo apt install -y ccache cmake python-is-python3
 
   # Install documentation related tools
-  sudo apt install -y doxygen graphviz plantuml ditaa pandoc mactex
+  sudo apt install -y doxygen graphviz plantuml ditaa pandoc mactex librsvg2-bin
 
   # Install useful utilities
-  sudo apt install -y dos2unix
+  sudo apt install -y dos2unix sqlite sqlitebrowser universal-ctags
   
   # Install X11 dev packages
   sudo apt install -y x11-apps x11-xserver-utils libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev
@@ -60,18 +65,23 @@ then
   # Update package manager
   brew update
 
+  # Essentials
+  brew install make gcc git
+
+  # Code quality tools
+  brew install cppcheck clang-format gcovr
+
   # Install cmake and other tools
   brew install ccache cmake python
 
   # Install documentation related tools
   brew install doxygen graphviz plantuml ditaa pandoc basictex librsvg
+
+  # Install useful utilities
+  brew install dos2unix sqlite db-browser-for-sqlite universal-ctags
   
   # Set python to python3
   brew link python@3.9
-
-  # Install modern ctags version
-  brew tap universal-ctags/universal-ctags
-  brew install --HEAD universal-ctags
 
   # For doc generation
   pip install pandoc-latex-environment
@@ -89,14 +99,47 @@ then
     echo 'export PATH="~/scoop/shims/:$PATH"' | tee -a ~/.bash_profile
   fi
 
+  # Add searching for extra packages
+  scoop bucket add extras
+
+  # Formula for rsvg-convert
+  scoop bucket add pomberger_pom https://github.com/pomberger/pom
+
   # Update package manager
   scoop update
 
+  # Essentials
+  scoop install make mingw git
+
+  # Code quality tools
+  scoop install cppcheck
+     # clang-format clang-tidy gcovr   are missing from scoop
+
   # Install cmake and other tools
-  scoop install make cmake python
+  scoop install ccache cmake python
 
   # Install documentation related tools
-  scoop install doxygen graphviz pandoc # TODO: miktex rsvg-convert
+  scoop install doxygen graphviz plantuml pandoc miktex rsvg-convert
+     # ditaa    is missing from scoop
+
+  # Install useful utilities
+  scoop install dos2unix sqlite sqlitebrowser universal-ctags
+
+  # Install more tools
+  scoop install ffmpeg jq neovim rust wget 
+
+  # Various desktop applications
+  # scoop install windows-terminal github vscode libreoffice inkscape gimp vcxsrv vlc sourcetree notepadplusplus obs-studio putty cdburnerxp clion crystaldiskinfo crystaldiskmark debugviewpp depends geekbench godot spacesniffer googlechrome handbrake hwinfo inno-setup irfanview kodi magicavoxel makemkv meld mqtt-spy msiafterburner skype
+ 
+  # Mostly already available through git-bash, seems installing the git package will install git-for-windows
+  # scoop install busybox curl grep gzip tar unzip which diffutils
+
+  # TODO: should try to install just mingw and git on a fresh windows install and see how
+  #       many commands that makes available without installing specifically the other commands
+
+  # Other possible packages
+  # scoop install llvm binutils cscope gcc gdb msys
+
 
   # Install other dev packages
   # scoop bucket add user https://github.com/excitoon/scoop-user.git
