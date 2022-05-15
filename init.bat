49b2be908f3a4bb8986d3914eacf8826d10f5fa5
John Ryland <JohnRyland@users.noreply.github.com>
2022-05-15 22:57:16 +1000

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,6 +1,8 @@
 @echo off
 
-powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; iwr -useb get.scoop.sh | iex"
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'Tls, Tls11, Tls12, Ssl3' ; Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; iwr -useb get.scoop.sh | iex"
+if not exist "init.sh" powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'Tls, Tls11, Tls12, Ssl3' ; iwr -useb https://raw.githubusercontent.com/JohnRyland/DevelopmentEnvironment/main/init.sh"
+
 set PATH=%USERPROFILE%\scoop\shims;%PATH%
 cmd.exe /c scoop install git
 %USERPROFILE%\scoop\apps\git\current\bin\bash -c ./init.sh
@@ -8,4 +10,3 @@ echo Done
 
 rem Show a shell when done
 %USERPROFILE%\scoop\apps\git\current\git-bash
-
