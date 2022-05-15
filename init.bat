5f9b2e1fa5841450880440b30e1c46e84519d4db
John Ryland <JohnRyland@users.noreply.github.com>
2022-05-15 23:43:21 +1000

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,6 +1,9 @@
 @echo off
 
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'Tls, Tls11, Tls12, Ssl3' ; Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; iwr -useb get.scoop.sh | iex"
+rem For Windows 10 1709, needed to do this, however I think the better idea is to update windows, keep checking and installing updates until it is past 1709
+rem powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'Tls, Tls11, Tls12, Ssl3' ; Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; iwr -useb get.scoop.sh | iex"
+
+powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; iwr -useb get.scoop.sh | iex"
 
 set PATH=%USERPROFILE%\scoop\shims;%PATH%
 cmd.exe /c scoop install git wget
