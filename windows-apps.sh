eaca7353e5324dc066ad731d0c2d79eb8497588d
John Ryland <JohnRyland@users.noreply.github.com>
2022-05-21 23:57:06 +1000

    Update windows-apps.sh

--- windows-apps.sh
+++ windows-apps.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Various common desktop applications
+scoop install clover libreoffice inkscape gimp vlc notepadplusplus cdburnerxp spacesniffer googlechrome irfanview skype
 
-scoop install clover
+# Various developer tools
+scoop install github vscode clion sourcetree putty depends debugviewpp meld mqtt-spy inno-setup
 
+# Media and 3D tools
+# scoop install obs-studio godot kodi msiafterburner makemkv magicavoxel handbrake
+
+# Benchmarking and diagnostics tools
+scoop install crystaldiskinfo crystaldiskmark geekbench hwinfo        
