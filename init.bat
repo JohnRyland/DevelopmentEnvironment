54d5f4881ac4fc5d8c0cfe1141cd521480c3d297
John Ryland <JohnRyland@users.noreply.github.com>
2022-05-24 00:55:31 +1000

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,18 +1,6 @@
 @echo off
-
 powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; iwr -useb get.scoop.sh | iex"
 set PATH=%USERPROFILE%\scoop\shims;%PATH%
 cmd.exe /C scoop install git wget
 if not exist "init.sh" wget https://raw.githubusercontent.com/JohnRyland/DevelopmentEnvironment/main/init.sh
-
 cmd.exe /C %USERPROFILE%\scoop\apps\git\current\bin\bash -c ./init.sh
-echo Done
-
-echo.
-echo *********************************************
-echo   Please now run ./init.sh in the terminal
-echo *********************************************
-echo.
-
-rem Show a shell when done
-%USERPROFILE%\scoop\apps\git\current\git-bash
