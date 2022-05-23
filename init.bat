b63847db159eb0bb2018d75e4ce8060021c41d6e
John Ryland <JohnRyland@users.noreply.github.com>
2022-05-23 22:22:27 +1000

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,16 +1,12 @@
 @echo off
 
-rem For Windows 10 1709, needed to do this, however I think the better idea is to update windows, keep checking and installing updates until it is past 1709
-rem powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'Tls, Tls11, Tls12, Ssl3' ; Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; iwr -useb get.scoop.sh | iex"
-
 powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; iwr -useb get.scoop.sh | iex"
-
 set PATH=%USERPROFILE%\scoop\shims;%PATH%
-cmd.exe /c scoop install git wget
+cmd.exe /C scoop install git wget
 if not exist "init.sh" wget https://raw.githubusercontent.com/JohnRyland/DevelopmentEnvironment/main/init.sh
 
-rem %USERPROFILE%\scoop\apps\git\current\bin\bash -c ./init.sh
-rem echo Done
+cmd.exe /C %USERPROFILE%\scoop\apps\git\current\bin\bash -c ./init.sh
+echo Done
 
 echo.
 echo *********************************************
