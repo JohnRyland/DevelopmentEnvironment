e6c2b4a718cda797c987986acf568ca6a7c53097
John Ryland <JohnRyland@users.noreply.github.com>
2022-05-24 00:54:39 +1000

    Update init.sh

--- init.sh
+++ init.sh
@@ -202,13 +202,12 @@ then
   echo 'dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart' >> setup_wsl.bat
   echo 'wsl --set-default-version 2' >> setup_wsl.bat
   echo 'wsl_update_x64.msi /passive' >> setup_wsl.bat
+  echo 'echo about to shutdown' >> setup_wsl.bat
+  echo 'pause' >> setup_wsl.bat
   echo 'shutdown /r' >> setup_wsl.bat
-  # sudo start cmd "//C wsl --install -d Ubuntu"
   echo '@echo off' > install_wsl.bat
-  # echo 'wsl --set-default-version 2' >> install_wsl.bat
   echo 'wsl --install -d Ubuntu' >> install_wsl.bat
   echo 'del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\install_wsl.bat"' >> install_wsl.bat
-  # echo 'pause' >> install_wsl.bat
   cp install_wsl.bat "$APPDATA/Microsoft/Windows/Start Menu/Programs/Startup/"
   
   print_banner "Please reboot for all changes to take effect"
